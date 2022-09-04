bc4daa8a638ad365d2bcad565528f799246514fb
BeZd4r <103284584+BeZd4r@users.noreply.github.com>
2022-09-04 22:24:52 +0300

    Fix groups and check all posts

--- VK_Parser.py
+++ VK_Parser.py
@@ -2,7 +2,7 @@ from openpyxl import load_workbook
 import requests
 import time
 
-names,page_url = [],[]
+names,page_ids ,page_url = [],[], []
 offset = 0
 total_count_post = 0
 
@@ -14,25 +14,20 @@ def Post_Checker(post_id):
         for objects in response["response"]["profiles"]:
             name = objects["first_name"]+" "+objects["last_name"]
             name_id = "https://vk.com/id"+str(objects["id"])
+            page_id = str(objects["id"])
             if name not in names:
                 names.append(name)
+                page_ids.append(page_id)
                 page_url.append(name_id)
 
-        reply_id = ""
-        for replies in response["response"]["items"]:
-            for reply in replies["thread"]["items"]:
-                if str(reply["from_id"]) not in reply_id:
-                    reply_id += str(reply["from_id"])+", "
-        else:
-            time.sleep(0.1)
-            url_users = f"https://api.vk.com/method/users.get?user_ids={reply_id}&access_token={token}&v=5.131"
-            response = requests.get(url_users).json()
-            for objectes in response["response"]:
-                name = str(objectes["first_name"])+" "+str(objectes["last_name"])
-                name_id = "https://vk.com/id"+str(objectes["id"])
-                if name not in names:
-                    names.append(name)
-                    page_url.append(name_id)
+        for objects in response["response"]["groups"]:
+            name = objects["name"]
+            name_id = "https://vk.com/id"+str(objects["id"])
+            page_id = str(objects["id"])
+            if name not in names:
+                names.append(name)
+                page_ids.append(page_id)
+                page_url.append(name_id)
 
         print(f"Post {post_id} is checked")
     except Exception:
@@ -55,15 +50,24 @@ def Wall_Checker():
 f = open("Server_Token_(VK_API).txt")
 token = f.read()
 
+url_wall = f"https://api.vk.com/method/wall.get?offset=0&owner_id=-212637974&extended=1&count=100&access_token={token}&v=5.131"
+response = requests.get(url_wall).json()
+total_count_post = response["response"]["count"]
+
 Wall_Checker()
-print(len(names))
 
 exel_file = "Table.xlsx"
 work_book = load_workbook(exel_file)
 work_book.remove(work_book["Data"])
 work_list = work_book.create_sheet("Data")
+work_list.column_dimensions['A'].width = 10
+work_list.column_dimensions['B'].width = 24
+work_list.column_dimensions['C'].width = 10
+work_list.column_dimensions['D'].width = 30
 for i in range(1,len(names)+1):
-    work_list[f"A{i}"] = names[i-1]
-    work_list[f"B{i}"] = page_url[i-1]
+    work_list[f"A{i}"] = i
+    work_list[f"B{i}"] = names[i-1]
+    work_list[f"C{i}"] = page_ids[i-1]
+    work_list[f"D{i}"] = page_url[i-1]
 work_book.save(exel_file)
 work_book.close()
