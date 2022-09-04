aa070ba46fa23f2448ff6380fd4881e7ba0ec888
BeZd4r <103284584+BeZd4r@users.noreply.github.com>
2022-09-04 14:39:49 +0300

    Fix Reply and Token
    Fix reply check and del my token

--- VK_Parser.py
+++ VK_Parser.py
@@ -4,42 +4,59 @@ import requests
 import time
 
 names,page_url = [],[]
-counter,offset = 0,0
-url = f"https://api.vk.com/method/wall.get?owner_id=-212637974&extended=1&count=100&access_token=vk1.a.EVGM5hYS6zzsfFYj5IuvZairWa2hM7oFfMWcWea3bRJZi7bcBS048d2xQUFSdqvcS6aLXb4tHG4XT20fCxXQ_1KKkCf-lSu676xGuKZc81zFIWrTPunpfp_GLGkqCejV9PH7gEMT2wFNd6ubpvlVAzbmVj0T62NftYJMPTWBoImLd-F15-vk597Vrj6zgtue&v=5.131"
-response = requests.get(url).json()
-total_count_post = response["response"]["count"]
+offset = 0
+total_count_post = 0
 
 def Post_Checker(post_id):
-    url_post = f"https://api.vk.com/method/wall.getComments?owner_id=-212637974&extended=1&post_id={post_id}&count=100&access_token=vk1.a.EVGM5hYS6zzsfFYj5IuvZairWa2hM7oFfMWcWea3bRJZi7bcBS048d2xQUFSdqvcS6aLXb4tHG4XT20fCxXQ_1KKkCf-lSu676xGuKZc81zFIWrTPunpfp_GLGkqCejV9PH7gEMT2wFNd6ubpvlVAzbmVj0T62NftYJMPTWBoImLd-F15-vk597Vrj6zgtue&v=5.131"
-    response = requests.get(url_post).json()
+    try:
+        url_post = f"https://api.vk.com/method/wall.getComments?thread_items_count=10&owner_id=-212637974&extended=1&post_id={post_id}&count=100&access_token={token}&v=5.131"
+        response = requests.get(url_post).json()
 
-    for objects in response["response"]["profiles"]:
-        name = objects["first_name"]+" "+objects["last_name"]
-        name_id = "https://vk.com/id"+str(objects["id"])
-        if name not in names:
-            names.append(name)
-            page_url.append(name_id)
+        for objects in response["response"]["profiles"]:
+            name = objects["first_name"]+" "+objects["last_name"]
+            name_id = "https://vk.com/id"+str(objects["id"])
+            if name not in names:
+                names.append(name)
+                page_url.append(name_id)
 
+        reply_id = ""
+        for replies in response["response"]["items"]:
+            for reply in replies["thread"]["items"]:
+                if str(reply["from_id"]) not in reply_id:
+                    reply_id += str(reply["from_id"])+", "
+        else:
+            time.sleep(0.1)
+            url_users = f"https://api.vk.com/method/users.get?user_ids={reply_id}&access_token={token}&v=5.131"
+            response = requests.get(url_users).json()
+            for objectes in response["response"]:
+                name = str(objectes["first_name"])+" "+str(objectes["last_name"])
+                name_id = "https://vk.com/id"+str(objectes["id"])
+                if name not in names:
+                    names.append(name)
+                    page_url.append(name_id)
+
+        print(f"Post {post_id} is checked")
+    except Exception:
+        print(f"Post {post_id} is broke")
 
 def Wall_Checker():
-    global total_count_post, offset, counter
-    url_wall = f"https://api.vk.com/method/wall.get?offset={offset}&owner_id=-212637974&extended=1&count=100&access_token=vk1.a.EVGM5hYS6zzsfFYj5IuvZairWa2hM7oFfMWcWea3bRJZi7bcBS048d2xQUFSdqvcS6aLXb4tHG4XT20fCxXQ_1KKkCf-lSu676xGuKZc81zFIWrTPunpfp_GLGkqCejV9PH7gEMT2wFNd6ubpvlVAzbmVj0T62NftYJMPTWBoImLd-F15-vk597Vrj6zgtue&v=5.131"
+    global total_count_post, offset
+    url_wall = f"https://api.vk.com/method/wall.get?offset={offset}&owner_id=-212637974&extended=1&count=100&access_token={token}&v=5.131"
     response = requests.get(url_wall).json()
 
     for item in response['response']['items']:
-        print (item['id'])
         Post_Checker(int(item['id']))
         time.sleep(0.25)
-        counter += 1
-        print(counter)
 
     if total_count_post > 0:
         offset += 100
         total_count_post -= 100
         Wall_Checker()
 
-Wall_Checker()
+f = open("Server_Token_(VK_API).txt")
+token = f.read()
 
+Wall_Checker()
 print(len(names))
 
 exel_file = "Table.xlsx"
